4e58e8ad5e57f884477121cc867f572c3985014d
Enes Başkaya <enesbaskaya08@gmail.com>
2018-07-23 19:13:42 +0300

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,34 +1,12 @@
-/*
-function dowork(data,callback){
 
-    callback("herşey yolunca");
-
-}
-
-function doworkpromise(data){
-     
-    return new Promise(function(resolve,reject){
-
-        reject("hata var lee");
-    })
-
-}
-
-
-doworkpromise("data").then(function(response){
-
-    console.log(response);
-},function(error){
-    console.log(error);
-
-})*/
 var request = require('request');
 
 function getweather(location){
     return new Promise(function(resolve,reject){
 
         var encodeURI= encodeURIComponent(location);
-        var url = "http://api.openweathermap.org/data/2.5/weather?q="+encodeURI+",tr&appid=5c1093a6f30891eda04dc3db21ea5e48&units=metric";
+        var Apikey="";
+        var url = "http://api.openweathermap.org/data/2.5/weather?q="+encodeURI+",tr&appid="+Apikey+"&units=metric";
     
         if(!location){
 
